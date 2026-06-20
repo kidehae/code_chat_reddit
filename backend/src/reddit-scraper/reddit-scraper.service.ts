64942d9f5109c3e842e89aad702d8114f3dd0610
kidehae <149243635+kidehae@users.noreply.github.com>
2026-06-20 11:54:12 +0300

    Fix deployment issues

--- backend/src/reddit-scraper/reddit-scraper.service.ts
+++ backend/src/reddit-scraper/reddit-scraper.service.ts
@@ -126,7 +126,7 @@ export class RedditScraperService implements OnModuleInit {
     
     this.logger.log(`Fetching RSS data stream from r/${subreddit}...`);
     
-    const response = await firstValueFrom(
+    const response = await firstValueFrom<any>(
       this.httpService.get(url, {
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
